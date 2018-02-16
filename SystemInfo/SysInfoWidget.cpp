2cdb09aa2d127e8b0d25180c276e5d7871216e37
Betrezen88 <michal.s.wyszynski@gmail.com>
2018-02-16 21:22:47 +0100

    Added class SysInfoWidget.

---
+++ SystemInfo/SysInfoWidget.cpp
@@ -0,0 +1,28 @@
+#include "SysInfoWidget.h"
+
+#include <QVBoxLayout>
+
+using namespace QtCharts;
+
+SysInfoWidget::SysInfoWidget(QWidget *parent, int startDelayMs, int updateSeriesDelayMs)
+    : QWidget( parent )
+{
+    m_chartView = new QChartView(this);
+    m_refreshTimer.setInterval( updateSeriesDelayMs );
+    connect(&m_refreshTimer, &QTimer::timeout, this, &SysInfoWidget::updateSeries);
+    QTimer::singleShot( startDelayMs,
+        [this] { m_refreshTimer.start(); } );
+
+    //m_chartView.setRenderHint( QPainter::Antialiasing );
+    //m_chartView.chart()->legend()->setVisible( false );
+
+    QVBoxLayout* layout = new QVBoxLayout(this);
+    layout->addWidget( m_chartView );
+
+    setLayout( layout );
+}
+
+QChartView* SysInfoWidget::chartView()
+{
+    return m_chartView;
+}
--- SystemInfo/SystemInfo.pro
+++ SystemInfo/SystemInfo.pro
@@ -4,7 +4,7 @@
 #
 #-------------------------------------------------
 
-QT       += core gui
+QT       += core gui charts
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -26,11 +26,13 @@ DEFINES += QT_DEPRECATED_WARNINGS
 SOURCES += \
         main.cpp \
         MainWindow.cpp \
-    SysInfo.cpp
+    SysInfo.cpp \
+    SysInfoWidget.cpp
 
 HEADERS += \
         MainWindow.h \
-    SysInfo.h
+    SysInfo.h \
+    SysInfoWidget.h
 
 windows {
     SOURCES += SysInfoWindowsImpl.cpp
