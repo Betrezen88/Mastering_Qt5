d7339a4665590816da6cb67df38e4159327f7ed2
Betrezen88 <michal.s.wyszynski@gmail.com>
2018-02-16 21:53:01 +0100

    Added basic logic to CpuWidget.

--- SystemInfo/CpuWidget.cpp
+++ SystemInfo/CpuWidget.cpp
@@ -5,7 +5,13 @@ using namespace QtCharts;
 CpuWidget::CpuWidget(QWidget *parent)
     : SysInfoWidget(parent), m_series(new QPieSeries(this))
 {
+    m_series->setHoleSize(0.35);
+    m_series->append("CPU Load", 30.0);
+    m_series->append("CPU Free", 70.0);
 
+    QChart* chart = chartView().chart();
+    chart->addSeries(m_series);
+    chart->setTitle("CPU average load");
 }
 
 void CpuWidget::updateSeries()
